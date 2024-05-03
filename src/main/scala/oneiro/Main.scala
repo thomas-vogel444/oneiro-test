c0d09bd765308b099eafb4722405247b3f35a978
thomasvogel <tom.vogel@yondercard.com>
2024-05-03 13:17:01 +0200

    Fixed issue whereby the in-memory map was getting reinitialised upon every command

--- src/main/scala/oneiro/Main.scala
+++ src/main/scala/oneiro/Main.scala
@@ -1,6 +1,6 @@
 package oneiro
 
-import cats.effect.kernel.Ref
+import cats.effect.kernel.{Ref, Resource}
 import cats.effect.{ExitCode, IO, IOApp}
 import oneiro.clients.LoanRepository
 import oneiro.domain.Loan
@@ -13,17 +13,17 @@ object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
     for {
       _ <- IO.println("Welcome to the Loan Interest Calculator.")
-      _ <- inputLoop
-    } yield ExitCode.Success
-
-  def inputLoop: IO[Command] =
-    for {
       ref <- Ref[IO].of(mutable.Map[String, Loan]())
       loanRepository = new LoanRepository(ref)
       loanService = new LoanService(loanRepository)
       console = new LoanConsole(loanService)
+      _ <- inputLoop(console)
+    } yield ExitCode.Success
+
+  def inputLoop(console: LoanConsole): IO[Command] =
+    for {
       command <- console.commandPrompt
       _ <- console.processCommand(command)
-      _ <- if (command != ExitConsole) inputLoop else IO.pure(ExitConsole)
+      _ <- if (command != ExitConsole) inputLoop(console) else IO.pure(ExitConsole)
     } yield command
 }
